e3cb8dfdeee2bcc8473288d7fbadf21368b9e72a
luzhengqin <2862410968@qq.com>
2023-04-12 01:17:58 +0800

    no message

--- testSqlite.php
+++ testSqlite.php
@@ -7,8 +7,15 @@ class MyDB extends SQLite3
         $this->open('diansai.db');
     }
 }
-
+$arr = array();
 $db = new MyDB();
 $result = $db->query('select * from test');
-var_dump( $result->fetchArray() );
+while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+    $user_id = $row['id'];
+    $name = $row['name'];
+    array_push($arr, array('user_id' => $user_id, 'name' => $name));
+}
+$result = array('code' => 0, 'msg' => '', 'count' => 2, 'data' => $arr);
+return json_encode($result);
+//var_dump( $result->fetchArray() );
 ?>
