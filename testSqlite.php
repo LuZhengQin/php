de53cfeaaf070a1fd1c2de31bc0cdd2befdf7041
LuZhengQin <2862410968@qq.com>
2023-04-12 02:07:43 +0800

    Update testSqlite.php

--- testSqlite.php
+++ testSqlite.php
@@ -1,21 +1,27 @@
 <?php
-class MyDB extends SQLite3
-{
-    function __construct()
-    {
-        //如果xx.db不存在的话则直接创建一个，存在的话直接使用。
-        $this->open('test.db');
-    }
-}
-$arr = array();
-$db = new MyDB();
-$result = $db->query('select * from test');
-while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-    $user_id = $row['id'];
-    $name = $row['name'];
-    array_push($arr, array('user_id' => $user_id, 'name' => $name));
-}
-$result = array('code' => 0, 'msg' => '', 'count' => 2, 'data' => $arr);
-return json_encode($result);
-//var_dump( $result->fetchArray() );
+   class MyDB extends SQLite3
+   {
+      function __construct()
+      {
+         $this->open('test.db');
+      }
+   }
+   $db = new MyDB();
+   if(!$db){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Opened database successfully\n";
+   }
+
+   $sql =<<<EOF
+      SELECT * from test;
+EOF;
+
+   $ret = $db->query($sql);
+   while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+      echo "ID = ". $row['id'] . "\n";
+      echo "NAME = ". $row['name'] ."\n";
+   }
+   echo "Operation done successfully\n";
+   $db->close();
 ?>
