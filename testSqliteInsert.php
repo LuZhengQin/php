c1b0ceca1288f97a370954664cba9b5b054c94df
LuZhengQin <2862410968@qq.com>
2023-04-12 22:42:19 +0800

    Update testSqliteInsert.php

--- testSqliteInsert.php
+++ testSqliteInsert.php
@@ -1,37 +1,35 @@
 <?php
-   class MyDB extends SQLite3
-   {
-      function __construct()
-      {
-         $this->open('test.db');
-      }
-   }
-   $db = new MyDB();
-   if(!$db){
-      echo $db->lastErrorMsg();
-   } else {
-      echo "Opened database successfully\n";
-   }
-   $sql =<<<EOF
-      UPDATE COMPANY set SALARY = 25000.00 where ID=1;
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('test.db');
+    }
+}
+$db = new MyDB();
+if(!$db){
+    echo $db->lastErrorMsg();
+} else {
+    echo "Opened database successfully\n";
+}
+$sql =<<<EOF
+      UPDATE test set name = lzq1 where ID=1;
 EOF;
-   $ret = $db->exec($sql);
-   if(!$ret){
-      echo $db->lastErrorMsg();
-   } else {
-      echo $db->changes(), " Record updated successfully\n";
-   }
+$ret = $db->exec($sql);
+if(!$ret){
+    echo $db->lastErrorMsg();
+} else {
+    echo $db->changes(), " Record updated successfully\n";
+}
 
-   $sql =<<<EOF
-      SELECT * from COMPANY;
+$sql =<<<EOF
+      SELECT * from test;
 EOF;
-   $ret = $db->query($sql);
-   while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
-      echo "ID = ". $row['ID'] . "\n";
-      echo "NAME = ". $row['NAME'] ."\n";
-      echo "ADDRESS = ". $row['ADDRESS'] ."\n";
-      echo "SALARY =  ".$row['SALARY'] ."\n\n";
-   }
-   echo "Operation done successfully\n";
-   $db->close();
+$ret = $db->query($sql);
+while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+    echo "ID = ". $row['ID'] . "\n";
+    echo "NAME = ". $row['NAME'] ."\n";
+}
+echo "Operation done successfully\n";
+$db->close();
 ?>
